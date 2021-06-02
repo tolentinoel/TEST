a3769f91eff8d7afb4798f829b694b3cd0ec1e44
tolentinoel <rckrleyn@gmail.com>
2021-06-01 17:13:54 -0700

    adding semantic ui components

--- netlify-test/src/App.js
+++ netlify-test/src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faAt} from '@fortawesome/free-solid-svg-icons';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import faker from 'faker';
+import {Card, Icon, Image} from 'semantic-ui-react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Will this be updated?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div className="ui container comments" >
+          <ApprovalCard>
+              <CommentDetail
+              author={faker.name.firstName()}
+              timeAgo="Today at 3:30pm"
+              image={faker.image.avatar()}
+              content="Awesome!"
+            />
+          </ApprovalCard>
+
+          <Card>
+        <Image src='https://semantic-ui.com/images/avatar2/large/elyse.png' wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>Elyse</Card.Header>
+            <Card.Meta>
+              <span className='date'>Joined in 2015</span>
+            </Card.Meta>
+            <Card.Description>
+              Elyse is a musician living in Nashville.
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a href="/">
+              <Icon name='user' />
+              22 Friends
+            </a>
+          </Card.Content>
+        </Card>
+
+          </div>
       </header>
     </div>
   );
