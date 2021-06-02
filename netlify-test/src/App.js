532d466added582359f7b6b306ee2819eeaa2214
tolentinoel <rckrleyn@gmail.com>
2021-06-01 18:07:06 -0700

    added Feed component and used faker avatars

--- netlify-test/src/App.js
+++ netlify-test/src/App.js
@@ -4,43 +4,45 @@ import './App.css';
 // import { faAt} from '@fortawesome/free-solid-svg-icons';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
-import faker from 'faker';
+
 import {Card, Icon, Image} from 'semantic-ui-react';
+import FeedExampleBasic from './Feed';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <div className="ui container comments" >
-          <ApprovalCard>
-              <CommentDetail
-              author={faker.name.firstName()}
-              timeAgo="Today at 3:30pm"
-              image={faker.image.avatar()}
-              content="Awesome!"
-            />
-          </ApprovalCard>
-
-          <Card>
-        <Image src='https://semantic-ui.com/images/avatar2/large/elyse.png' wrapped ui={false} />
-          <Card.Content>
-            <Card.Header>Elyse</Card.Header>
-            <Card.Meta>
-              <span className='date'>Joined in 2015</span>
-            </Card.Meta>
-            <Card.Description>
-              Elyse is a musician living in Nashville.
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <a href="/">
-              <Icon name='user' />
-              22 Friends
-            </a>
-          </Card.Content>
+        <div className="ui container comments" >
+        {/* <ApprovalCard>
+            <CommentDetail
+            author={faker.name.firstName()}
+            timeAgo="Today at 3:30pm"
+            image={faker.image.avatar()}
+            content="Awesome!"
+          />
+        </ApprovalCard> */}
+        
+        <img src="https://semantic-ui.com/images/avatar2/large/elyse.png" className="ui medium circular image" alt="avatar"/>
+        <Card>
+          {/* <Image className="ui medium circular image" src='https://semantic-ui.com/images/avatar2/large/elyse.png' circular ui={false} /> */}
+            <Card.Content>
+              <Card.Header>Elyse</Card.Header>
+              <Card.Meta>
+                <span className='date'>Joined in 2015</span>
+              </Card.Meta>
+              <Card.Description>
+                Elyse is a musician living in Nashville.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a href="/">
+                <Icon name='user' />
+                22 Friends
+              </a>
+            </Card.Content>
         </Card>
-
-          </div>
+        <FeedExampleBasic />
+        </div>
       </header>
     </div>
   );
---
+++ netlify-test/src/Feed.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import { Feed, Icon } from 'semantic-ui-react';
+import faker from 'faker';
+
+const FeedExampleBasic = () => (
+  <Feed>
+    <Feed.Event>
+      <Feed.Label>
+        <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' alt='avatar'/>
+      </Feed.Label>
+      <Feed.Content>
+        <Feed.Summary>
+          <Feed.User>Elliot Fu</Feed.User> added you as a friend
+          <Feed.Date>1 Hour Ago</Feed.Date>
+        </Feed.Summary>
+        <Feed.Meta>
+          <Feed.Like>
+            <Icon name='like' />4 Likes
+          </Feed.Like>
+        </Feed.Meta>
+      </Feed.Content>
+    </Feed.Event>
+
+    <Feed.Event>
+      {/* <Feed.Label image='https://semantic-ui.com/images/avatar/small/ade.jpg' /> */}
+      <Feed.Label image={faker.image.avatar()} />
+      <Feed.Content>
+        <Feed.Summary
+          date='2 Days Ago'
+          user='Ade Hess'
+          content='add you as a friend'
+        />
+        <Feed.Meta>
+          <Feed.Like>
+            <Icon name='like' />8 Likes
+          </Feed.Like>
+        </Feed.Meta>
+      </Feed.Content>
+    </Feed.Event>
+
+    <Feed.Event>
+      <Feed.Label image='https://semantic-ui.com/images/avatar/small/joe.jpg' />
+      <Feed.Content>
+        <Feed.Summary>
+          <a href='/'>Joe Henderson</a> posted on his page
+          <Feed.Date>3 days ago</Feed.Date>
+        </Feed.Summary>
+        <Feed.Extra text>
+          Ours is a life of constant reruns. We're always circling back to where
+          we'd we started, then starting all over again. Even if we don't run
+          extra laps that day, we surely will come back for more of the same
+          another day soon.
+        </Feed.Extra>
+        <Feed.Meta>
+          <Feed.Like>
+            <Icon name='like' />5 Likes
+          </Feed.Like>
+        </Feed.Meta>
+      </Feed.Content>
+    </Feed.Event>
+
+    <Feed.Event>
+      <Feed.Label image='https://semantic-ui.com/images/avatar/small/justen.jpg' />
+      <Feed.Content>
+        <Feed.Summary>
+          <a href="/">Justen Kitsune</a> added <a href='/'>2 new photos</a> of you
+          <Feed.Date>4 days ago</Feed.Date>
+        </Feed.Summary>
+        <Feed.Extra images>
+          <a href='/'>
+            <img src='https://react.semantic-ui.com/images/wireframe/image.png' alt="avatar" />
+          </a>
+          <a href ='/'>
+            <img src='https://react.semantic-ui.com/images/wireframe/image.png' alt="avatar"/>
+          </a>
+        </Feed.Extra>
+        <Feed.Meta>
+          <Feed.Like>
+            <Icon name='like' />
+            41 Likes
+          </Feed.Like>
+        </Feed.Meta>
+      </Feed.Content>
+    </Feed.Event>
+  </Feed>
+)
+
+export default FeedExampleBasic
