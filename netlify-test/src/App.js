ca046ad87beb5e418c420b2bd867306bd9484ddd
tolentinoel <rckrleyn@gmail.com>
2021-06-02 10:44:45 -0700

    updated netlify cli, adjusted main avatar size

--- netlify-test/src/App.js
+++ netlify-test/src/App.js
@@ -11,12 +11,12 @@ function App() {
   return (
     <div className="App">
       <div className="topNav">
-        Yay! you checked out my practice page! Whatchu think? - tolentinoel
+        Yay! you checked out my practice page! Whatchu think? - ellaine.dev
       </div>
       <header className="App-header">
         <div className="ui container comments" id="mainContainer">
 
-        <img src="https://semantic-ui.com/images/avatar2/large/elyse.png" className="ui medium circular image" alt="avatar"/>
+        <img id="mainAvatar" src="https://semantic-ui.com/images/avatar2/large/elyse.png" className="ui medium circular image" alt="avatar"/>
         <div className="bannerDiv">
           banner
         </div>
