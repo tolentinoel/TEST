effeff8d856d04e80aac75d522df6211d4a2c93f
tolentinoel <rckrleyn@gmail.com>
2021-06-06 00:06:13 -0700

    adjusting code. cleanup

--- netlify-test/src/App.js
+++ netlify-test/src/App.js
@@ -1,27 +1,20 @@
 import './App.css';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faAt} from '@fortawesome/free-solid-svg-icons';
-// import CommentDetail from './CommentDetail';
-// import ApprovalCard from './ApprovalCard';
 
-import {Card, Icon, Image} from 'semantic-ui-react';
+import {Card, Icon} from 'semantic-ui-react';
 import FeedExampleBasic from './Feed';
 
 function App() {
   return (
     <div className="App">
-      <div className="topNav">
-        Yay! you checked out my practice page! Whatchu think? - ellaine.dev
-      </div>
+      <div className="topNav">Yay! you checked out my practice page! Whatchu think? - ellaine.dev</div>
       <header className="App-header">
         <div className="ui container comments" id="mainContainer">
-
-        <img id="mainAvatar" src="https://semantic-ui.com/images/avatar2/large/elyse.png" className="ui medium circular image" alt="avatar"/>
+          <img id="mainAvatar" src="https://semantic-ui.com/images/avatar2/large/elyse.png" className="ui medium circular image" alt="avatar"/>
         <div className="bannerDiv">
-          banner
-        </div>
+          banner</div>
         <Card>
-          {/* <Image className="ui medium circular image" src='https://semantic-ui.com/images/avatar2/large/elyse.png' circular ui={false} /> */}
             <Card.Content>
               <Card.Header>Elyse</Card.Header>
               <Card.Meta>
@@ -40,14 +33,6 @@ function App() {
         </Card>
         <FeedExampleBasic />
 
-         {/* <ApprovalCard>
-            <CommentDetail
-            author={faker.name.firstName()}
-            timeAgo="Today at 3:30pm"
-            image={faker.image.avatar()}
-            content="Awesome!"
-          />
-        </ApprovalCard> */}
         </div>
       </header>
     </div>
